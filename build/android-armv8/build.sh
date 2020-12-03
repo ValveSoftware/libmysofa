7fa94043faffca98842bf3b3157506b6efe48274
Lakulish Antani <lakulisha@valvesoftware.com>
2020-12-03 13:46:39 -0800

    Update build scripts to allow building shared libraries, or debug static libraries

--- build/android-armv8/build.sh
+++ build/android-armv8/build.sh
@@ -9,16 +9,28 @@ echo Using NDK at $NDK.
 
 NDK_MAKE=$NDK/prebuilt/windows-x86_64/bin/make.exe
 
+MYSOFA_CMAKE_BUILD_TYPE=Release
+if [[ "$MYSOFA_BUILD_DEBUG" == "true" ]]; then
+    MYSOFA_CMAKE_BUILD_TYPE=Debug
+fi
+
+MYSOFA_BUILD_SHARED_LIBS=FALSE
+MYSOFA_TARGET=mysofa-static
+if [[ "$MYSOFA_BUILD_SHARED" == "true" ]]; then
+    MYSOFA_BUILD_SHARED_LIBS=TRUE
+    MYSOFA_TARGET=mysofa-shared
+fi
+
 cmake \
     -G "Unix Makefiles" \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=$MYSOFA_CMAKE_BUILD_TYPE \
     -DCMAKE_MAKE_PROGRAM=$NDK_MAKE \
     -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
     -DANDROID_ABI=arm64-v8a \
     -DANDROID_PLATFORM=21 \
     -DANDROID_STL=c++_static \
     -DBUILD_TESTS=FALSE \
-    -DBUILD_SHARED_LIBS=FALSE \
+    -DBUILD_SHARED_LIBS=$MYSOFA_BUILD_SHARED_LIBS \
     ../..
 
-$NDK_MAKE mysofa-static
+$NDK_MAKE $MYSOFA_TARGET
